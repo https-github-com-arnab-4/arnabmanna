a7ec927c98bcc4ead96784b440df34cca20b5937
Arnab Manna <107208391+arnab-4@users.noreply.github.com>
2023-02-27 22:15:40 +0530

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,6 @@
 // This is my new project
 console.log("Hello World")
 console.log("Hello World")// Another one
+
+console.log("Thanks for giving me the badges github ")
+//ok here is the last commit from my organization
